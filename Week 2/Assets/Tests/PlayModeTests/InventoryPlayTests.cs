1b62716ae3a301b4f69cf2b144f13ac333a8a562
Charlie Campbell <chacha9669@hotmail.com>
2025-04-14 19:21:40 +1000

    DragItemToNewInventoryAndSwapItems test

--- Week 2/Assets/Tests/PlayModeTests/InventoryPlayTests.cs
+++ Week 2/Assets/Tests/PlayModeTests/InventoryPlayTests.cs
@@ -111,4 +111,136 @@ public class InventoryPlayTests
 
         yield return null;
     }
+
+    [UnityTest]
+    public IEnumerator DragItemToNewInventoryAndSwapItems()
+    {
+        // Arrange: Set up 2 Inventories, both with their own UI and 1 item
+        // Act: Simulate dragging a DraggableItem from one inventory to another
+        // Assert: Verify the items swapped
+        
+        //Arrange
+        
+        // Create the UI canvas
+        GameObject canvasGameObject = new GameObject("Canvas");
+        Canvas canvas = canvasGameObject.AddComponent<Canvas>();
+        canvas.renderMode = RenderMode.ScreenSpaceOverlay;
+        canvasGameObject.AddComponent<CanvasScaler>();
+        canvasGameObject.AddComponent<GraphicRaycaster>();
+        
+        // Add event system for drag events
+        if (EventSystem.current == null)
+        {
+            new GameObject("EventSystem", typeof(EventSystem), typeof(StandaloneInputModule));
+        }
+        
+        // Create the inventory UI game object with InventoryUI component
+        GameObject inventory1UIGameObject = new GameObject("InventoryUI");
+        //Make it a child of the Canvas
+        inventory1UIGameObject.transform.SetParent(canvas.transform);
+        GridLayoutGroup gridLayoutGroup1 = inventory1UIGameObject.AddComponent<GridLayoutGroup>();
+        gridLayoutGroup1.cellSize = new Vector2(64, 64);
+        gridLayoutGroup1.spacing = new Vector2(2, 2);
+        gridLayoutGroup1.startCorner = GridLayoutGroup.Corner.UpperLeft;
+        gridLayoutGroup1.startAxis = GridLayoutGroup.Axis.Horizontal;
+        gridLayoutGroup1.childAlignment = TextAnchor.MiddleCenter;
+        gridLayoutGroup1.constraint = GridLayoutGroup.Constraint.FixedColumnCount;
+        gridLayoutGroup1.constraintCount = 4;
+        InventoryUI inventory1UI = inventory1UIGameObject.AddComponent<InventoryUI>();
+        
+        // Create the inventory game object (one that holds the data)
+        GameObject inventory1GameObject = new GameObject("Inventory");
+        Inventory inventory1Component = inventory1GameObject.AddComponent<Inventory>();
+        
+        //Assign the inventory to the inventory UI
+        inventory1UI.inventory = inventory1Component;
+        
+        //Init the items array for InventoryUI
+        inventory1UI.inventory.items = new InventoryItem[1];
+        inventory1UI.inventory.items[0] = ScriptableObject.CreateInstance<InventoryItem>();
+        inventory1UI.inventory.items[0].name = "Sword";
+        
+        // Load the slot and item prefabs needed to initialise the InventoryUI
+        inventory1UI.slotPrefab = Resources.Load<InventorySlot>("Prefabs/InventorySlot");
+        inventory1UI.itemPrefab = Resources.Load<DraggableItem>("Prefabs/InventoryItem");
+        
+        // Create the inventory UI game object with InventoryUI component
+        GameObject inventory2UIGameObject = new GameObject("InventoryUI2");
+        //Make it a child of the Canvas
+        inventory2UIGameObject.transform.SetParent(canvas.transform);
+        GridLayoutGroup gridLayoutGroup2 = inventory2UIGameObject.AddComponent<GridLayoutGroup>();
+        gridLayoutGroup2.cellSize = new Vector2(64, 64);
+        gridLayoutGroup2.spacing = new Vector2(2, 2);
+        gridLayoutGroup2.startCorner = GridLayoutGroup.Corner.UpperLeft;
+        gridLayoutGroup2.startAxis = GridLayoutGroup.Axis.Horizontal;
+        gridLayoutGroup2.childAlignment = TextAnchor.MiddleCenter;
+        gridLayoutGroup2.constraint = GridLayoutGroup.Constraint.FixedColumnCount;
+        gridLayoutGroup2.constraintCount = 4;
+        InventoryUI inventory2UI = inventory2UIGameObject.AddComponent<InventoryUI>();
+        
+        // Create the inventory game object (one that holds the data)
+        GameObject inventory2GameObject = new GameObject("Inventory2");
+        Inventory inventory2Component = inventory2GameObject.AddComponent<Inventory>();
+        
+        //Assign the inventory to the inventory UI
+        inventory2UI.inventory = inventory2Component;
+        
+        //Init the items array for InventoryUI
+        inventory2UI.inventory.items = new InventoryItem[1];
+        
+        inventory2UI.inventory.items[0] = ScriptableObject.CreateInstance<InventoryItem>();
+        inventory2UI.inventory.items[0].name = "Shield";
+        
+        // Load the slot and item prefabs needed to initialise the InventoryUI
+        inventory2UI.slotPrefab = Resources.Load<InventorySlot>("Prefabs/InventorySlot");
+        inventory2UI.itemPrefab = Resources.Load<DraggableItem>("Prefabs/InventoryItem");
+        
+        // Call initialise() to setup the inventory ui
+        inventory1UI.Init();
+        inventory2UI.Init();
+        
+        // set positions
+        inventory1UI.transform.position = new Vector3(-288, 0, 0);
+        inventory2UI.transform.position = new Vector3(288, 0, 0);
+        
+        //Act
+        
+        //Simulate dragging from slot 0
+        PointerEventData pointerEventData = new PointerEventData(EventSystem.current);
+        
+        //Set initial pointer position to the centre of the draggable item
+        RectTransform draggableItemRect = inventory1UI.slots[0].draggableItem.GetComponent<RectTransform>();
+        Vector3 startPosition = draggableItemRect.position;
+        pointerEventData.position = startPosition;
+        
+        // Start dragging
+        inventory1UI.slots[0].draggableItem.OnBeginDrag(pointerEventData);
+        //We need to yield to allow the Event system to update
+        yield return null;
+        // Simulate dragging over to the target slot (in the 2nd inventory)
+        RectTransform targetSlotRect = inventory2UI.slots[0].GetComponent<RectTransform>();
+        
+        //Hover mouse over target position
+        pointerEventData.position = targetSlotRect.position;
+        
+        inventory1UI.slots[0].draggableItem.OnDrag(pointerEventData);
+        
+        //We need to yield to allow the Event system to update
+        yield return null;
+        
+        // End dragging
+        inventory1UI.slots[0].draggableItem.OnEndDrag(pointerEventData);
+        
+        yield return null;
+        //Assert
+        
+        //Check if items are swapped
+        Assert.AreEqual("Shield", inventory1UI.inventory.items[0].name);
+        Assert.AreEqual("Shield", inventory1UI.slots[0].draggableItem.item.name);
+        
+        Assert.AreEqual("Sword", inventory2UI.inventory.items[0].name);
+        Assert.AreEqual("Sword", inventory2UI.slots[0].draggableItem.item.name);
+
+        yield return null;
+    }
 }
