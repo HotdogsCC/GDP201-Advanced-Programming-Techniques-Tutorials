c5784463002160d868aef681940fc13234cf1995
Charlie Campbell <chacha9669@hotmail.com>
2025-05-03 23:27:15 +1000

    equip sword and shield

---
+++ Week 2/Assets/Scripts/InventoryEquip.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryEquip : MonoBehaviour
+{
+    [Header("Inventory Reference")]
+    [SerializeField] private Inventory playerInventory;
+    
+    [Header("Equip Object References")]
+    [SerializeField] private GameObject swordGameObject;
+    [SerializeField] private GameObject shieldGameObject;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        swordGameObject.SetActive(false);
+        shieldGameObject.SetActive(false);
+        playerInventory.OnItemTransaction += TryToEquip;
+    }
+
+    void TryToEquip(InventoryItem item, string transactionType)
+    {
+        switch (transactionType)
+        {
+            case "buy":
+                if (item.itemName.Contains("Sword"))
+                {
+                    swordGameObject.SetActive(true);
+                }
+
+                else if (item.itemName.Contains("Shield"))
+                {
+                    shieldGameObject.SetActive(true);
+                }
+                break;
+            
+            case "sell":
+                if (item.itemName.Contains("Sword"))
+                {
+                    swordGameObject.SetActive(false);
+                }
+
+                else if (item.itemName.Contains("Shield"))
+                {
+                    shieldGameObject.SetActive(false);
+                }
+                break;
+        }
+        
+        
+    }
+}
